78e4e2612e80aed60d69d167f64c16ce5c4a7879
Ian Pratt <ianjdpratt@gmail.com>
2021-12-20 14:09:03 -0600

    chagnes ig

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,27 @@
 module Main where
 
+-- interpereter structure
+-- lexical analysis -> syntactical analysis -> ??? -> evaluation -> out
+
+data Expr
+  = Number Int
+  | Add Expr Expr
+  | Sub Expr Expr
+  -- | Var Ident
+  -- | Let Ident Expr Expr
+
+data Value = NumVal Int
+
+eval :: Expr -> Value
+eval (Number x) = NumVal x
+eval (Add x y) = let NumVal x' = eval x in
+                 let NumVal y' = eval y in
+                 NumVal(x' + y')
+eval (Sub x y) = let NumVal x' = eval x in
+                 let NumVal y' = eval y in
+                 NumVal(x' - y')
+
+
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
